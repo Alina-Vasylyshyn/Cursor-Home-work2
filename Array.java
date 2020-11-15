3c2773daac806623323d461ce5811d92b004396a
Alina-Vasylyshyn <71761033+Alina-Vasylyshyn@users.noreply.github.com>
2020-11-15 19:01:36 +0200

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,16 @@
+package Cursor;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Array {
+    public static void main(String[] args) {
+        Integer[] array = {2,3,1,7,11};
+        Arrays.sort(array, Collections.reverseOrder());
+        for(int i:array) {
+            System.out.println(i);
+        }
+
+    }
+}
